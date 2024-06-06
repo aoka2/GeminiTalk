033b60b0be06bf28fa9c34567a147879177d5ef2
aoka2 <aoka543222@gmail.com>
2024-06-06 09:54:43 +0900

    test add
    testファイルを追加、ファイル内容は把握していません
    気になる方は各個人で解決をお願いします。

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ https://ai.google.dev/gemini-api/docs/get-started/python
 """
 
 import os
+import AIVoice
 
 import google.generativeai as genai
 
@@ -46,6 +47,11 @@ model = genai.GenerativeModel(
   safety_settings=safety_settings,
   generation_config=generation_config,
 )
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
 
 
 # response = chat_session.send_message("")
@@ -58,19 +64,21 @@ model = genai.GenerativeModel(
 # gemini_pro = genai.GenerativeModel("gemini-1.5-pro-latest")
 
 name = "山田"
-hobby = "プロサッカー"
+hobby = "ゲーム"
 job = "システムエンジニア"
-MBTI = "明るめ"
+MBTI = "やんちゃ"
 gender = "男性"
 
 text ="デートで異性と会った時のコミュニケーションの練習に付き合ってください。恋愛シュミレーションゲームのヒロインのように話してくれれば大丈夫です。\n あなたは女性役です。私と受け答えをしてください\n返答の文章はできるだけ人間に寄せてください。\n 私のMBTI診断の結果に合わせて喋り方を変えてください。\n以下がプロフィールです。\n性別："+gender+"\n名前："+name+"\n 趣味："+hobby+"\n仕事："+job+"\n性格："+MBTI
 text += "\nまた、アドバイスなどの人間味の無い提案は、恋愛シュミレーションゲームでは全く行われないので、こちらが求めていなければ出さないようにしてください。"
+text += "\n待ち合わせ場所は岡山駅です。"
 text += "\nここで、質問は一回に一つだけです。"
 
-text +="\n以下は女性役であるあなたのプロフィールです。このプロフィールに従って会話をしてください。\n性別：女性\n 名前：あい\n 趣味：マッチングアプリ\n仕事：IT企業のマネージャー\n あなたの性格：ヒステリック\n一人称：私"
+text +="\n以下は女性役であるあなたのプロフィールです。このプロフィールに従って会話をしてください。\n性別：女性\n 名前：あい\n 趣味：ゲーム\n仕事：IT企業のマネージャー\n あなたの性格：犬系彼女\n一人称：私"
+text += "\n恋愛シュミレーションゲームのような会話をする際に、情報が足りない場合は、足りない情報を具体的に私に質問してください。私がその情報を渡します。"
 
-print('\'end\' と打てばチャットが終了します。')
-response = chat_session.send_message(text + "\nはじめまして！")
+print('\'end\' と打てばチャットが終了します。' + text)
+response = chat_session.send_message(text)
 print(response.text)
 while True:
     # プロンプトの設定
@@ -85,4 +93,6 @@ while True:
 
         # 生成結果の表示
         print(response.text)
+        AIVoice.create_Post(response.text)
+        
 
